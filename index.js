67e7cf55516906f6a930ddcbc5c464efa54d180d
Candousek <candousek@email.cz>
2022-07-24 21:18:29 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const { GClient, Plugins, Command, Component } = require('gcommands');
+const { Intents } = require('discord.js');
+const { join } = require('path');
+
+// Set the default cooldown for commands
+Command.setDefaults({
+	cooldown: '3s',
+});
+
+// Set the default onError function for components
+Component.setDefaults({
+	onError: (ctx, error) => {
+		return ctx.reply('Oops! Something went wrong')
+	} 
+});
+
+Plugins.search(__dirname);
+
+const client = new GClient({
+	dirs: [
+		join(__dirname, 'commands'),
+		join(__dirname, 'components'),
+		join(__dirname, 'listeners')
+	],
+	messageSupport: true,
+	messagePrefix: '!',
+	devGuildId: process.env.DEV_SERVER,
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+// Login to the discord API
+client.login(process.env.TOKEN);

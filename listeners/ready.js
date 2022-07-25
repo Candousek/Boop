eca205231002a6747e4c1fbf7ef9cd4fdb501e6e
Candousek <candousek@email.cz>
2022-07-25 13:22:52 +0200

    Update ready

---
+++ listeners/ready.js
@@ -0,0 +1,16 @@
+const { Listener } = require("gcommands");
+
+new Listener({
+    name: "ready",
+    event: "ready",
+    run: async (client) => {
+
+        client.guilds.cache.map(async guild => {
+            await guild.members.fetch();
+            await guild.roles.fetch();
+            console.log(`Fetched members and roles from ${guild.name}`)
+        })
+
+
+    },
+});

2ab5844d0f409c1990d4f74145052511f756f885
Candousek <candousek@email.cz>
2022-07-25 13:22:45 +0200

    Update findmembers

--- commands/moderation/findmember.js
+++ commands/moderation/findmember.js
@@ -15,19 +15,21 @@ new Command({
             required: true
         }
     ],
-	run: (ctx) => {
+	run: async (ctx) => {
+
+        await ctx.deferReply();
 		
         const query = ctx.arguments.getString("nickname");
-        const filtered_members = ctx.guild.members.cache.filter(member => member.user.username.includes(query));
+        const filtered_members = ctx.guild.members.cache.filter(member => member.user.username.toLowerCase().includes(query.toLowerCase()));
 
         if (filtered_members.size === 0) {
-            return ctx.reply("No members found.");
+            return ctx.safeReply(`No members found for query ${query}.`);
         };
 
-        fs.writeFileSync(`./data.txt`, `*****************************\n  MEMBER IDS FOR ${query}\n*****************************\n${filtered_members.map(member => member.id).join(",")}\n\n*****************************\n MEMBER IDS WITH NICKNAMES\n*****************************\n${filtered_members.map(member => `${member.id} | ${member.user.tag}`).join("\n")}`);
+        fs.writeFileSync(`./data.txt`, `*****************************\n  MEMBER IDS LIST (for massban)\n*****************************\n${filtered_members.map(member => member.id).join(",")}\n\n*****************************\n MEMBER IDS WITH NICKNAMES\n*****************************\n${filtered_members.map(member => `${member.id} | ${member.user.tag}`).join("\n")}`);
 
-        ctx.reply({
-            content: `Found ${filtered_members.size} member${filtered_members.size > 1 ? "s" : ""}.`,
+        ctx.safeReply({
+            content: `Found ${filtered_members.size} member${filtered_members.size > 1 ? "s" : ""} for query ${query}.`,
             files: [ "./data.txt" ]
         })
 

438f546eb20b79542a3b27b17fc9318ff9a14441
Candousek <candousek@email.cz>
2022-07-30 17:04:02 +0200

    Added new module

---
+++ modules/config.js
@@ -0,0 +1,16 @@
+const database = require("./database");
+
+module.exports = {
+    get: async function(key) {
+        const result = (await database.query("SELECT * FROM config WHERE configkey=?", [key]))[0];
+        return result.configvalue;
+    },
+    set: async function(key, value) {
+        await database.query("INSERT INTO config (configkey, configvalue) VALUES (?, ?) ON DUPLICATE KEY UPDATE configvalue = ?", [key, value, value]);
+        return true;
+    },
+    delete: async function(key) {
+        await database.query("DELETE FROM config WHERE configkey = ?", [key]);
+        return true;
+    },
+}

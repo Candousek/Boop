0976352b448988326b147f738bb47c5193780ef8
Candousek <candousek@email.cz>
2022-08-01 11:21:39 +0200

    Update config.js

--- modules/config.js
+++ modules/config.js
@@ -1,9 +1,9 @@
-const database = require("./database");
+const database = require("./db");
 
 module.exports = {
     get: async function(key) {
         const result = (await database.query("SELECT * FROM config WHERE configkey=?", [key]))[0];
-        return result.configvalue;
+        return result?.configvalue;
     },
     set: async function(key, value) {
         await database.query("INSERT INTO config (configkey, configvalue) VALUES (?, ?) ON DUPLICATE KEY UPDATE configvalue = ?", [key, value, value]);
@@ -13,4 +13,8 @@ module.exports = {
         await database.query("DELETE FROM config WHERE configkey = ?", [key]);
         return true;
     },
+    getAll: async function() {
+        const result = await database.query(`SELECT * FROM config`);
+        return result;
+    }
 }

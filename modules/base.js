ebf5abe60e446daadbc9f05bf7bbe12ece002595
Candousek <candousek@email.cz>
2022-08-05 16:43:26 +0200

    Update base.js

--- modules/base.js
+++ modules/base.js
@@ -1,13 +1,21 @@
 const { WebhookClient } = require("discord.js");
 
 module.exports = {
-    log: async function(client, text) {
+    log: async function(client, options) {
         const webhook_client = new WebhookClient({ url: client.configs.id.logs_webhook_url });
 
-        webhook_client.send({
-            content: text,
-            username: client.user.username,
-            avatarURL: client.user.displayAvatarURL()
-        })
+        options.username = client.user.username
+        options.avatarURL = client.user.displayAvatarURL()
+
+        webhook_client.send(options)
+    },
+
+    replaceInObject: function(object, replaceInfo) {
+        let stringified = JSON.stringify(object);
+        for(const [key, value] of Object.entries(replaceInfo)) {
+            stringified = stringified.replaceAll(key, value);
+        }
+            
+        return JSON.parse(stringified);
     }
 }
